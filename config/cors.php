8ec0e07036d8c0046b1c31af305ccb3c264f1f0d
sunnya40a <sunnya_o@yahoo.com>
2025-08-03 21:10:56 +0545

    Reconfigured 2025

--- config/cors.php
+++ config/cors.php
@@ -24,7 +24,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://aarushi.net:5173',
-        'http://192.168.1.11:5173'
+        'http://192.168.1.11:5173',
+        'http://127.0.0.1:5173'
     ],
 
     //'allowed_origins_patterns' => [],
